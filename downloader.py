13573317355231019c70ef782f5e72ab4b563ae9
Jen <jennifer.stemkowski@gmail.com>
2024-04-15 10:51:40 -0400

    Add entry for video url

--- downloader.py
+++ downloader.py
@@ -14,4 +14,12 @@ app_label = Label(root, text="Video Downloader", fg="blue", font=("Arial", 20))
 canvas.create_window(200, 20, window=app_label)
 
 
+# Entry to accept video URL
+url_label = Label(root, text="Enter video URL: ", font=("Arial", 12))
+url_entry = Entry(root)
+canvas.create_window(200, 80, window=url_label)
+canvas.create_window(200, 100, window=url_entry)
+
+
+
 root.mainloop()
