5dee380b4cca26db0c70fdc0cd9bec1539c5c806
Jen <jennifer.stemkowski@gmail.com>
2024-04-15 10:47:58 -0400

    Add app label to tkinter window

--- downloader.py
+++ downloader.py
@@ -9,5 +9,9 @@ root.title("Video Downloader")
 canvas = Canvas(root, height=300, width=400)
 canvas.pack()
 
+# App label
+app_label = Label(root, text="Video Downloader", fg="blue", font=("Arial", 20))
+canvas.create_window(200, 20, window=app_label)
+
 
 root.mainloop()
