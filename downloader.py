b9b49aaa35993150987c0382f788e14cfa415a5f
Jen <jennifer.stemkowski@gmail.com>
2024-04-15 17:22:39 -0400

    Add functionality for mp3 conversion

--- downloader.py
+++ downloader.py
@@ -2,15 +2,26 @@ from tkinter import *
 from tkinter import filedialog
 from pytube import YouTube
 from moviepy.editor import *
+import shutil
 
 
 # Function to download video
 def download_video():
     video_path = url_entry.get()
     file_path = path_label.cget("text")
+    print("Downloading video...")
     mp4 = YouTube(video_path).streams.get_highest_resolution().download()
     video_clip = VideoFileClip(mp4)
+
+    # Code for mp3 conversion
+    audio_file = video_clip.audio_file
+    audio_file.write_audiofile("audio.mp3")
+    audio_file.close()
+    shutil.move("audio.mp3", file_path)
+
     video_clip.close()
+    shutil.move(mp4, file_path)
+    print("Download complete!")
 
 
 # Function to get path
@@ -27,7 +38,7 @@ canvas = Canvas(root, height=300, width=400)
 canvas.pack()
 
 # App label
-app_label = Label(root, text="Video Downloader", fg="blue", font=("Arial", 20))
+app_label = Label(root, text="Video Downloader", fg='blue', font=("Arial", 20))
 canvas.create_window(200, 20, window=app_label)
 
 
@@ -39,7 +50,7 @@ canvas.create_window(200, 100, window=url_entry)
 
 
 # Path to download videos
-path_label = Label(root, text="Select path to save video to: ", font=("Arial", 12))
+path_label = Label(root, text="Select where to save video to: ", font=("Arial", 12))
 path_button = Button(root, text="Select", command=get_path)
 canvas.create_window(200, 150, window=path_label)
 canvas.create_window(200, 170, window=path_button)
