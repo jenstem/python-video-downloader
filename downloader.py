90ec5eba7f8c837720196bba2cf360bb37d047ea
Jen <jennifer.stemkowski@gmail.com>
2024-04-15 11:00:59 -0400

    Add path + btn for video download

--- downloader.py
+++ downloader.py
@@ -21,5 +21,11 @@ canvas.create_window(200, 80, window=url_label)
 canvas.create_window(200, 100, window=url_entry)
 
 
+# Path to download videos
+path_label = Label(root, text="Select path to download video: ", font=("Arial", 12))
+path_button = Button(root, text="Select")
+canvas.create_window(200, 150, window=path_label)
+canvas.create_window(200, 170, window=path_button)
+
 
 root.mainloop()
