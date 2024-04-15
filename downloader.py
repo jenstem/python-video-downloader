e520ee0275e8785b8df0f82e48970e0a84e69d87
Jen <jennifer.stemkowski@gmail.com>
2024-04-15 10:45:14 -0400

    Set up Tkinter window

---
+++ downloader.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import filedialog
+
+
+root = Tk()
+root.title("Video Downloader")
+
+
+canvas = Canvas(root, height=300, width=400)
+canvas.pack()
+
+
+root.mainloop()
